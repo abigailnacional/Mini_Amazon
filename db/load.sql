b51b2a78882bd36e3f2e86c1c9d5d6e471b46249
caleb watson <caw117@duke.edu>
2021-10-07 15:21:17 -0400

    loading remaining tables from e/r

--- db/load.sql
+++ db/load.sql
@@ -1,3 +1,7 @@
 \COPY Users FROM 'data/Users.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Product FROM 'data/Products.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY Purchase FROM 'data/Purchases.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Cart FROM 'data/Cart.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Feedback FROM 'data/Feedback.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY ProductsInCart FROM 'data/ProductsInCart.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Sell FROM 'data/Sellss.csv' WITH DELIMITER ',' NULL '' CSV

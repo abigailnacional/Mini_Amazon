efe8a04d5d9762ec1cce2db8466289c14e7eb747
Leah Schwartz <ls323@duke.edu>
2021-10-29 09:15:23 -0400

    Added comment for product in cart id

--- app/models/product_in_cart.py
+++ app/models/product_in_cart.py
@@ -19,7 +19,7 @@ class ProductInCart:
         self.seller_id = seller_id
 
     @staticmethod
-    def increase_quantity(product_in_cart_id: int):
+    def increase_quantity(product_in_cart_id: int):  # product_in_cart_id refers to the id in the ProductInCart table
         app.db.execute_with_no_return(
             """
             UPDATE ProductInCart

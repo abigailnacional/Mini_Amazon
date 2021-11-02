bb5003dd82cdce241a36dfe7f6590afc242c05d9
Leah Schwartz <ls323@duke.edu>
2021-11-02 14:44:02 -0400

    Remove unneccessary purchase code

--- app/__init__.py
+++ app/__init__.py
@@ -27,9 +27,6 @@ def create_app():
     from .cart import bp as cart_bp
     app.register_blueprint(cart_bp)
 
-    from .purchases import bp as purchases_bp
-    app.register_blueprint(purchases_bp)
-
     from .orders import bp as orders_bp
     app.register_blueprint(orders_bp)
 

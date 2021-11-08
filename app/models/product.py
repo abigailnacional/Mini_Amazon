123daff73452790012bf18d1de95a31193929b97
Caleb Watson <caw117@duke.edu>
2021-11-08 10:46:42 -0500

    product pages can now be filtered

--- app/models/product.py
+++ app/models/product.py
@@ -34,16 +34,17 @@ WHERE id = :id
                               id=id)
         return Product(*(rows[0])) if rows is not None else None
 
+# method to return products served by a specific on-campus resturant venue
     @staticmethod
-    def get_specific(category):
+    def get_specific(seller_affiliation):
         rows = app.db.execute('''
 SELECT DISTINCT id, name, description, category, price, is_available, link, creator_id, image
 FROM Product
 RIGHT OUTER JOIN Sells ON Product.id=Sells.product_id
-WHERE seller_affiliation = :category AND is_available = True
+WHERE seller_affiliation = :seller_affiliation AND is_available = True
 ORDER BY id
 ''',
-                                category=category)
+                                seller_affiliation=seller_affiliation)
         return [Product(*row) for row in rows] if rows is not None else None                 
 
     @staticmethod
@@ -55,3 +56,27 @@ WHERE is_available = :is_available
 ''',
                               is_available=is_available)
         return [Product(*row) for row in rows]
+
+    @staticmethod
+    def get_categories(is_available=True):
+        rows = app.db.execute('''
+SELECT DISTINCT category
+FROM Product
+WHERE is_available = :is_available
+''',
+                                is_available=is_available)
+        return rows 
+
+# method to return filtered view of venue products by category (entrées, beverages, etc.)
+    @staticmethod
+    def filtered(seller_affiliation, category):
+        rows = app.db.execute('''
+SELECT DISTINCT id, name, description, category, price, is_available, link, creator_id, image
+FROM Product
+RIGHT OUTER JOIN Sells ON Product.id=Sells.product_id
+WHERE seller_affiliation = :seller_affiliation AND is_available = True AND category = :category
+ORDER BY id
+''',
+                              seller_affiliation=seller_affiliation,
+                              category=category)
+        return [Product(*row) for row in rows]                   

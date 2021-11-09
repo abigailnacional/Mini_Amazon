562e7f6a6e25528dbdbe03b4baaca8da5aed100c
Caleb Watson <caw117@duke.edu>
2021-11-09 16:44:37 -0500

    edit to seller table

--- app/products.py
+++ app/products.py
@@ -7,12 +7,12 @@ from .models.user import User
 from flask import Blueprint
 bp = Blueprint('products', __name__)
 
-product_sellers = {'1': 'Beyu Blue', '2': 'The Loop', '3': 'McDonalds', '4': 'Panda Express', '5': 'Il Forno',
-                   '6': 'Sazón'}
+product_sellers = {1: 'Beyu Blue', 2: 'The Loop', 3: 'McDonalds', 4: 'Panda Express', 5: 'Il Forno',
+                   6: 'Sazón'}
 
 @bp.route('/product', methods=['GET'])
 def view_product():
-    vender_id = request.args.get('id')
+    vender_id = int(request.args.get('id'))
     products = Product.get_specific(vender_id)
     categories = Product.get_categories()
     return render_template('product.html', vender_id=vender_id, product_sellers=product_sellers, 
@@ -20,7 +20,7 @@ def view_product():
 
 @bp.route('/filter', methods=['GET'])
 def filtered_view():
-    vender_id = request.args.get('id')
+    vender_id = int(request.args.get('id'))
     spec_category = request.args.get('cat')
     products = Product.filtered(vender_id, spec_category)
     categories = Product.get_categories()
@@ -29,9 +29,10 @@ def filtered_view():
 
 @bp.route('/view', methods=['GET'])
 def ind_view():
-    prod_id = request.args.get('id')
+    prod_id = int(request.args.get('id'))
     product = Product.get(prod_id)
     sellers = User.get_sellers(prod_id)
-    return render_template('ind_prod.html', product_info=product, sellers=sellers)
+    return render_template('ind_prod.html', product_info=product, sellers=sellers, 
+                            product_sellers=product_sellers)
     
 
